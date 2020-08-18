d2de7fa815f002a0dcf7a6662d707373a36b328f
Coleman Hallom <challom@pharmalogistics.com>
2020-08-18 07:54:38 -0500

    Added title to select list.

--- vuePage.js
+++ vuePage.js
@@ -2,9 +2,13 @@ const app = new Vue({
     el: '#app',
     data: {
       device: '', 
+
       errorTypes:[
+
         {text:'Bad Battery'}, 
+
         {text: 'Screen Crack'}
+        
       ]
     }
   })
