f9a19133ad0a0747fa17fcc51b129558b1371e07
Coleman Hallom <challom@pharmalogistics.com>
2020-08-18 14:02:23 -0500

    comments.

--- vuePage.js
+++ vuePage.js
@@ -14,13 +14,28 @@ const app = new Vue({
       methods:{
         errorSub : function() {
           alert(this.selectedText);
+          // Uses the returned text in an alert.
+
         }, 
+        //https://stackoverflow.com/questions/44117064/access-selected-text-not-value-of-a-select-element-with-vue
         updateFromSelectedText(event) {
+          // The @"METHOD" style in vuejs looks to set the method to METHOD(this). 
+          
+          
           const options = event.target.options;
+          // Creates a list of the options. 
+          
+          
+
           const selectedOption = options[options.selectedIndex];
-    
+          // Creates a variable that hold sthe selected index based on the change. 
+
+
           this.selectedText = selectedOption.textContent;
-          return this.selectedText;
+          // Holds the currently selected text. 
+
+          return this.selectedText.trim();
+          // Returns the text for use elsewhere. 
       }
       }
   })
